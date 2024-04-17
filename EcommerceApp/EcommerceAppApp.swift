051225ec02607387df95a6a34d2dd360719755b5
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-04-17 10:32:03 -0300

    Initial Commit

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by Sebastian Marquez Rojas on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
