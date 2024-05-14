df5552e8d6085319d14aa10085d43f05ed6c6c2d
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-05-13 22:12:57 -0300

    waiting for dynamic island

--- EcommerceApp/Components/DynamicIslandDelegate.swift
+++ EcommerceApp/Components/DynamicIslandDelegate.swift
@@ -4,12 +4,17 @@
 //
 //  Created by Sebastian Marquez Rojas on 11/05/2024.
 //
-
+/*
 import Foundation
 import ActivityKit
 
 final class DynamicIslandDelegate{
      func GetProductQuantity(quantity:[ProductModel]) throws -> Int{
         guard ActivityAuthorizationInfo().areActivitiesEnabled else {return 0}
+         let initialState = DynamicIslandModel.ContentState(quantity: quantity)
+         let futureDate:Date = .now + 3600
+         let activityContent = ActivityContent(state: initialState , staleDate: futureDate)
+         
     }
 }
+*/
