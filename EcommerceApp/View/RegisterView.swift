88d11631bb5d8153c28b42355a5d474e38966ad3
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-05-29 23:17:12 -0300

    register view created. design and logic in progress.

---
+++ EcommerceApp/View/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  EcommerceApp
+//
+//  Created by Sebastian Marquez Rojas on 29/05/2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
