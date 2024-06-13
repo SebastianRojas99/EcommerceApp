d20f2042a99a40291515c68816afb46fc914a6c2
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-06-13 17:36:51 -0300

    icons transition added.

--- EcommerceApp/View/CategoryListView.swift
+++ EcommerceApp/View/CategoryListView.swift
@@ -7,50 +7,38 @@
 
 import SwiftUI
 
-struct CategoryListView:View{
-    
+
+struct CategoryListView: View {
     @State var viewModel = CategoryViewModel(categoryList: categoryList)
     @Binding var selectedCategory: String
-    @State private var isExpanded: Bool = false
     
-    
-   var body : some View {
-       HStack{
-           ScrollView(.horizontal,showsIndicators: false){
-               HStack{
-                   ForEach(viewModel.categoryList,id: \.self){ item in
-                       Button{
-                           selectedCategory = item.title
-                           isExpanded = true
-                          
-                       }label: {
-                           if !isExpanded{
-                               HStack{
-                                   if item.title !=  "All"{
-                                       Image(systemName: item.icon).foregroundStyle(selectedCategory == item.title ? .brown : .black)
-                                   }else{
-                                       Text("All")
-                                   }
-                               }
-                               }else{
-                                   HStack{
-                                       if item.title !=  "All"{
-                                           Image(systemName: item.icon).foregroundStyle(selectedCategory == item.title ? .brown : .black)
-                                       }
-                                       Text(item.title)
-                                   }
-                               }
-                           
-                           
-                       }
-                       .padding()
-                       .background(selectedCategory == item.title ? item.color.opacity(0.4) : .gray.opacity(0.3))
-                       .foregroundStyle(.black)
-                       .clipShape(.capsule)
-                   }
-               }
-           }
-       }.padding(.horizontal,12)
-   }
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(viewModel.categoryList) { item in
+                    Button(action: {
+                        selectedCategory = item.title
+                    }) {
+                        HStack {
+                            if item.title != "All" {
+                                Image(systemName: item.icon)
+                                    .foregroundStyle(selectedCategory == item.title ? .brown : .black)
+                                if selectedCategory == item.title {
+                                    Text(item.title)
+                                }
+                            } else {
+                                Text("All")
+                            }
+                        }
+                        .padding()
+                        .background(selectedCategory == item.title ? item.color.opacity(0.4) : .gray.opacity(0.3))
+                        .foregroundStyle(.black)
+                        .clipShape(Capsule())
+                        .animation(.easeInOut, value: selectedCategory)
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 12)
+    }
 }
-
